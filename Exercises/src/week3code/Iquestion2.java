672d985bf5074b3301983c05882cdf995ca101aa
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2016-02-04 20:14:51 +0000

    Week3 exercises

---
+++ Exercises/src/week3code/Iquestion2.java
@@ -0,0 +1,6 @@
+package week3code;
+
+public interface Iquestion2 {
+	int number = 0;
+	String mystring = new String();
+}
---
+++ Exercises/src/week3code/Question2.java
@@ -0,0 +1,5 @@
+package week3code;
+
+public class Question2 implements Iquestion2 {
+	
+}
